0609c1f3920fc515c2e143ce09cb4672e4aa745b
Alande Amorim <alande@hubii.com.br>
2025-02-26 06:28:01 -0300

    chore: update folder structure

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '../adapters/controllers/app.controller';
+import { AppService } from '../application/usecases/app.service';
 
 @Module({
   imports: [],
